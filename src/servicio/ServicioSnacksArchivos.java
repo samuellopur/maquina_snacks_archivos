f660de5a1aea4a27daae0bd1bb92746ba80c88b0
Samuel Lopez <samuellopez01@hotmail.com>
2024-11-04 13:04:26 -0500

    Metodo obtenerSnacks

--- src/servicio/ServicioSnacksArchivos.java
+++ src/servicio/ServicioSnacksArchivos.java
@@ -5,6 +5,9 @@ import dominio.Snack;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,7 @@ public class ServicioSnacksArchivos implements ServicioSnacks{
         try {
             existe = archivo.exists();
             if (existe){
-                //this.snacks = obtenerSnacks();
+                this.snacks = obtenerSnacks();
             }
             else {
 //                Se crea el archivo sino existe
@@ -44,6 +47,24 @@ public class ServicioSnacksArchivos implements ServicioSnacks{
         this.agregarSnack(new Snack("Papas", 70.0));
         this.agregarSnack(new Snack("Refresco", 50.0));
         this.agregarSnack(new Snack("Sandwich", 120.0));
+    }
+
+    private List<Snack> obtenerSnacks(){
+        var snacks = new ArrayList<Snack>();
+        try {
+            List<String> lineas = Files.readAllLines(Paths.get(NOMBRE_ARCHIVO));
+            for (String linea: lineas) {
+                String[] lineaSnack = linea.split(","); //parseo separado por coma
+                var idSnack = lineaSnack[0]; //No se usa
+                var nombre = lineaSnack[1];
+                var precio = Double.parseDouble(lineaSnack[2]);
+                var snack = new Snack(); //Agregamos el snack vacio a la lista
+            }
+        }catch (Exception e){
+            System.out.println("Error al leer archivo de snacks: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return snacks;
     }
 
     @Override
